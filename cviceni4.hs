5ab1d0d777990fe7cd8bbaf45e19238124d64dfb
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2019-04-23 16:03:06 +0200

    Add fourth excercise.

--- cviceni4.hs
+++ cviceni4.hs
@@ -54,25 +54,19 @@ demoPrvniZaznam (Zaznam _ jm _:_) = jm
 {- Ukol zacina tady. Misto `undefined` dodejte definice funkci, ktere z logu
  - vytahnou pozadovany vysledek. -}
 serazeniUzivatele :: Zaznamy -> [Uzivatel]
-serazeniUzivatele (list) = sort(nub( map (\(Zaznam _ jm _) -> jm ) list))
+serazeniUzivatele list = (sort . nub) [uzivatel | Zaznam _ uzivatel _ <- list]
 
 top10vyber :: Zaznamy -> [(Cas, Castka)]
-top10vyber(list) = 
-  take 10 (reverse (sortBy (compare `on` snd)
-  (zip
-        (pouzeCas list)
-        (map( \(Vyber cena) -> cena) (filter jeVyber (pouzeOperace list))))))
-  
+top10vyber(list) =  (take 10 . reverse . sortBy (compare `on` snd)) [(cas, castka)| Zaznam cas _ (Vyber castka) <- list]
+
 top10pripis :: Zaznamy -> [(Uzivatel, Castka)]
-top10pripis (list) = take 10 (sortBy (compare `on` snd) (zip 
-    (pouzeJmeno list)
-    (map (\(Pripis castka) -> castka) (filter jePripis (pouzeOperace list)))))
+top10pripis (list) = (take 10 . sortBy (compare `on` snd)) [ (jmeno, castka) | Zaznam _ jmeno (Pripis castka) <- list]
 
 topUzivatel :: Zaznamy -> [(Int, Uzivatel)]  
-topUzivatel (list) = take 1 (reverse(sortBy (compare `on` fst) (frequency(pouzeJmeno list))))
+topUzivatel (list) = (take 1 . reverse . sortBy (compare `on` fst) . frequency) [jmeno | Zaznam _ jmeno _ <- list]
 
 topPrirustek :: Zaznamy -> [(Integer, Uzivatel)]
-topPrirustek (list) = take 1 (reverse (sortBy (compare `on` fst) (zip (map (`vydelek` list) (serazeniUzivatele list)) (serazeniUzivatele list))))
+topPrirustek (list) = (take 1 .reverse . sortBy (compare `on` fst)) (zip (map (`vydelek` list) (serazeniUzivatele list)) (serazeniUzivatele list))
 
 -- prumer vyberu spocitejte zaokrouhleny dolu
 prumerVyberuJ :: Zaznamy -> [(Integer, Uzivatel)]
@@ -86,37 +80,13 @@ neprihlaseneVybery = undefined
 
 
 -- Pomocne funkce
-jeVyber :: Operace -> Bool
-jeVyber (Vyber _) = True
-jeVyber _ = False
-
-pouzeOperace :: Zaznamy -> [Operace]
-pouzeOperace (list) = map ( \(Zaznam _ _ operace) -> operace) list
-
-pouzeCas :: Zaznamy -> [Cas]
-pouzeCas (list) = map ( \(Zaznam cas _ _) -> cas) list
-
-pouzeJmeno :: Zaznamy -> [Uzivatel]
-pouzeJmeno (list) = map ( \(Zaznam _ uzivatel _) -> uzivatel) list
-
-jePripis :: Operace -> Bool
-jePripis (Pripis _) = True
-jePripis _ = False
-
 frequency :: Ord a => [a] -> [(Int,a)] 
 frequency list = map (\l -> (length l, head l)) (group (sort list))
 
-pouzeOperaceJmena :: Uzivatel -> Zaznamy -> [Operace]
-pouzeOperaceJmena uzivatel list = [operace | (Zaznam _ uzivatel1 operace ) <- list, uzivatel == uzivatel1]
-
-sectiVyber :: [Operace] -> Integer
-sectiVyber list = sum( map ( \(Vyber castka) -> castka) list ) * (-1)
-
-sectiPripis :: [Operace] -> Integer
-sectiPripis list = sum( map ( \(Pripis castka) -> castka) list )
-
 vydelek :: Uzivatel -> Zaznamy -> Integer
-vydelek uzivatel list = 
-  let pripisy = filter jePripis (pouzeOperaceJmena uzivatel list)
-      vybery = filter jeVyber (pouzeOperaceJmena uzivatel list)
-  in sectiVyber(vybery) + sectiPripis(pripisy)
+vydelek hledanyUzivatel list = 
+  let pripisy = [castka | Zaznam _ uzivatel (Pripis castka) <- list, uzivatel == hledanyUzivatel]
+      vybery = [castka | Zaznam _ uzivatel (Vyber castka) <- list, uzivatel == hledanyUzivatel]
+  in sum(pripisy) - sum(vybery)
+
+
